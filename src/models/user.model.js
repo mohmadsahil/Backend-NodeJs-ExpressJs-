6bf3aa5a84ba2531b63bba020aaed7ada67922b6
Sahil <Sahilsid2001@gmail.com>
2024-02-14 19:18:09 +0530

    User & Video Models Has Been Created

---
+++ src/models/user.model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema =new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        index:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+    },
+    fullname:{
+        type:String,
+        required:true,
+        trim:true,
+        index:true
+    },
+    avatar:{
+        type:String,
+        required:true,
+    },
+    coverImage:{
+        type:String,
+    },
+    watchHistory:[{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Video",
+    }],
+    password:{
+        type:String,
+        required:true,
+    },
+    refreshToken:{
+        type:String,
+    }
+
+},{timestamps:true}) 
+
+export const User = mongoose.model("User",userSchema);
