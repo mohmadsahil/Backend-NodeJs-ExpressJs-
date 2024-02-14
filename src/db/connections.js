c5f4f945fa6750fa06b1472472bdf2f80dc18b3e
Sahil <Sahilsid2001@gmail.com>
2024-02-14 10:27:23 +0530

    MongoDB Error Resolved

--- src/db/connections.js
+++ src/db/connections.js
@@ -3,8 +3,8 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async ()=>{
     try{
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log("MongoDB Has Been Connected");
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`MongoDB Has Been Connected ${connectionInstance.connection.host}`);
     }
     catch(error){
         console.log("MongoDB Error",error);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import dotenv from "dotenv"; //Importhed .env package
+import dotenv from "dotenv"; //Imported .env package
 dotenv.config({ path:"./env"})
 import express from "express"
 import connectDB from "./db/connections.js";
